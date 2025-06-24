556e069440258863cf63a4b9881260373b3fa6ea
HashirRaees <hashirrais13@gmail.com>
2025-06-24 18:12:18 +0500

    bubble sorting

---
+++ app.js
@@ -0,0 +1,22 @@
+// Task 23/06/2025
+var sorting = [4, 6, 3, 1, 9];
+console.log(sorting);
+
+function sort() {
+  for (let i = 0; i < sorting.length; i++) {
+    for (let j = 0; j < sorting.length; j++) {
+      if (sorting[j] > sorting[j + 1]) {
+        //swap the numbers
+        let swap = sorting[j]; // let 1 = [4,6,3,1,9]
+        sorting[j] = sorting[j + 1]; // let 2 = [3,4,6,1,9]
+        sorting[j + 1] = swap; //let 3 = [3,4,1,6,9]
+        //let 4 =[3,1,4,6,9]
+        //let 5 = [1,3,4,6,9]
+      }
+    }
+  }
+  return sorting;
+}
+sort(sorting);
+console.log(sorting);
+// result = [1, 3, 4, 6, 9];
